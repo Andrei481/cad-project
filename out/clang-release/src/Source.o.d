32b2a75928ee9ec47bfc91914b7041f104dc62b2
Simion-Gheorghe-Leontino Inișconi <simioninisconi@gmail.com>
2023-11-25 22:35:37 +0200

    Updated .gitignore

--- out/clang-release/src/Source.o.d
+++
@@ -1,6 +0,0 @@
-../out/clang-release/src/Source.o: Source.cc Source.h \
-  C:/Users/Ini/Documents/omnetpp-6.0.1/include/omnetpp.h
-
-Source.h:
-
-C:/Users/Ini/Documents/omnetpp-6.0.1/include/omnetpp.h:
--- out/clang-release/src/myQ.o.d
+++
@@ -1,6 +0,0 @@
-../out/clang-release/src/myQ.o: myQ.cc myQ.h \
-  C:/Users/Ini/Documents/omnetpp-6.0.1/include/omnetpp.h
-
-myQ.h:
-
-C:/Users/Ini/Documents/omnetpp-6.0.1/include/omnetpp.h:
